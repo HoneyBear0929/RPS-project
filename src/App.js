138b4e058dd19f1dcaf0de60c27048bfc7fe3d6a
Jae hyeok <hihyuk5317@gmail.com>
2023-02-24 20:29:00 +0900

    RPS project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import Box from "./component/Box";
 import "./App.css";
 import { useState } from "react";
 
-//1. 박스2개
-//2. 가위 바위 보 버튼
-//3. 버튼 클릭 시  클릭한 값이 박스에 보임
-//4. 컴퓨터는 랜덤하게 아이템 선택이 됨
-//5. 3 4의 결과를 가지고 누가 이겼는지 승패를 따진다.
-//6. 승패결과에 따라 테두리 색이 바뀜
 
 const choice = {
   rock:{
